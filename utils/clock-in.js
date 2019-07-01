fd7bf5dd82c8b01c7f6b74278d797d791cb07abf
Laisson Rangel Silveira <laisson.silveira@digitro.com.br>
2019-07-01 12:33:49 -0300

    Correção

--- utils/clock-in.js
+++ utils/clock-in.js
@@ -32,23 +32,23 @@ class ClockIn {
                 this._setDate();
             });
             this._totalCalc(ci);
-            ci.divergences.sort(this._compareDate);
+            ci.divergences.sort(ClockIn._compareDate);
         });
-        return this.clockIn.sort(this._compareDivergenceDate);
+        return this.clockIn.sort(ClockIn._compareDivergenceDate);
     }
 
-    _compare(d01, d02) {
+    static _compare(d01, d02) {
         if (d01.isBefore(d02)) return 1;
         if (d01.isAfter(d02)) return -1;
         return 0;
     }
     
-    _compareDate(d01, d02){
-        return this._compare(d01.date, d02.date);
+    static _compareDate(d01, d02){
+        return ClockIn._compare(d01.date, d02.date);
     }
     
-    _compareDivergenceDate(d01, d02) {
-        return this._compare(d01.divergences[0].date, d02.divergences[0].date);
+    static _compareDivergenceDate(d01, d02) {
+        return ClockIn._compare(d01.divergences[0].date, d02.divergences[0].date);
     }
 
     _normalizeHour(hour) {
